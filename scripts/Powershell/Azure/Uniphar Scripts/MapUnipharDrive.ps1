46bf5c53cef2279dc271a25676839f0c9f0f76cb
Joe Hollings <39950257+joehollings@users.noreply.github.com>
2024-05-30 09:15:45 +0100

    Update MapUnipharDrive.ps1

--- scripts/Powershell/Azure/Uniphar Scripts/MapUnipharDrive.ps1
+++ scripts/Powershell/Azure/Uniphar Scripts/MapUnipharDrive.ps1
@@ -1,7 +1,7 @@
 ﻿$connectTestResult = Test-NetConnection -ComputerName 10107afs01.file.core.windows.net -Port 445
 if ($connectTestResult.TcpTestSucceeded) {
     # Save the password so the drive will persist on reboot
-    cmd.exe /C "cmdkey /add:`"10107afs01.file.core.windows.net`" /user:`"localhost\10107afs01`" /pass:`"eTYBy7jARtMTfvU40G+8sQ1gxcO6K9pItvCPHkYmEln55BaOzL81/pGtITqJPXxlrW+BnbNB7Ufy+AStKl+q5A==`""
+    #cmd.exe /C "cmdkey /add:`"10107afs01.file.core.windows.net`" /user:`"localhost\10107afs01`" /pass:`"`""
     # Mount the drive
     New-PSDrive -Name Z -PSProvider FileSystem -Root "\\10107afs01.file.core.windows.net\shared" -Persist
 } else {
